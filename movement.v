b7d28b98166a82008506d2a49f1b78b279ff7172
tgnielse1145 <tgnielse1145@gmail.com>
2020-11-27 11:34:16 -0700

    just playing around
    just playing around

--- movement.v
+++ movement.v
@@ -25,16 +25,24 @@ pulse clk_div (
 localparam FETCH_X = 3'd0, SAVE_X = 3'd1;
 localparam FETCH_Y = 3'd2, SAVE_Y = 3'd3;
 localparam FETCH_M = 3'd4, SAVE_M = 3'd5, SAVE_M_STANDING = 3'd6;
+localparam OBJ_X = 3'd8, SAVEOBJ_X = 3'd9;
+localparam OBJ_Y = 3'd10, SAVEOBJ_Y = 3'd11;
+localparam OBJ_M = 3'd12, SAVEOBJ_M = 3'd13, SAVEOBJ_M_STANDING = 3'd14;
 localparam IDLE = 3'd7;
 reg [2:0] PS, NS;
 
 
 localparam MARIO_X_END = 500;
+localparam OBJ_X_END = 500;
 
 localparam MARIO_STANDING   = 0;
 localparam MARIO_WALK_START = 1;
 localparam MARIO_WALK_MID   = 2;
 localparam MARIO_WALK_END   = 3;
+localparam OBJ_STANDING   = 100;
+localparam OBJ_WALK_START = 101;
+localparam OBJ_WALK_MID   = 102;
+localparam OBJ_WALK_END   = 103;
 reg inc_in, inc_out;
 
 // dff for mario walking animation (increment or decrement walk pattern)
@@ -104,9 +112,13 @@ always @(PS, data_in, inc_out) begin
 				end
 				
 				MARIO_WALK_MID: begin
+				$display("here i am in MARIO_WALK_MID and here is inc_out %d \n ", inc_out);
+				$display("here i am in MARIO_WALK_MID and here is inc_in %d \n ", inc_in);
 					if (inc_out) data_out = MARIO_WALK_START;
 					else		data_out = MARIO_WALK_END;
 					inc_in = inc_out;
+					$display("here i am in MARIO_WALK_MID2 and here is inc_out %d \n ", inc_out);
+				$display("here i am in MARIO_WALK_MID2 and here is inc_in %d \n ", inc_in);
 				end
 				
 				MARIO_WALK_END: begin
@@ -124,11 +136,63 @@ always @(PS, data_in, inc_out) begin
 		
 		SAVE_M_STANDING: begin
 			addr = 'h1002;
-			data_out = MARIO_STANDING;
+			//data_out = MARIO_STANDING;
+			data_out = OBJ_X;
+			$display("here i am in SAVE_M_STANDING and are we gonna go to OBJ_X \n");
 			we = 1'b1;
-			NS = IDLE;
+			NS = OBJ_X;
+		end
+		OBJ_X: begin
+			addr = 'h1003;
+			NS = SAVEOBJ_X;
 		end
 		
+		SAVEOBJ_X: begin
+			addr = 'h1003;
+			data_out = data_in + 3'd4;
+			we = 1'b1;
+			if (data_in < OBJ_X_END)
+				NS = OBJ_M;	// I don't want to change Y
+			else
+				NS = SAVEOBJ_M_STANDING;
+		end
+		OBJ_M: begin
+			addr = 'h1005;
+			NS = SAVEOBJ_M;
+		end
+		
+		SAVEOBJ_M: begin
+			addr = 'h1005;
+			we = 1'b1;
+			case (data_in)
+				OBJ_STANDING: begin
+					data_out = OBJ_WALK_START;
+					inc_in = 1'b1;
+				end
+				
+				OBJ_WALK_START: begin
+					data_out = OBJ_WALK_MID;
+					inc_in = 1'b1;
+				end
+				
+				OBJ_WALK_MID: begin
+					if (inc_out) data_out = OBJ_WALK_START;
+					else		data_out = OBJ_WALK_END;
+					inc_in = inc_out;
+				end
+				
+				OBJ_WALK_END: begin
+					data_out = OBJ_WALK_MID;
+					inc_in = 1'b0;
+				end
+				
+				default: begin
+					data_out = 'hx;
+					inc_in = 1'bx;
+				end
+			endcase
+			NS = OBJ_X;
+		end
 		IDLE: begin
 			NS = IDLE;
 		end
